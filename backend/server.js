66d0c04b407afc93d912def82f6e94001744684a
Syed Ibrahim Ali <syedibrahimali1111@gmail.com>
2026-01-11 13:23:31 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://nolu-f781.vercel.app",
+    credentials: true, 
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection (for Vercel deployment)
